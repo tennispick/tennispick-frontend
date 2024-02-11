8f6a1dbdf8acac02327e5125e538f5a863faee3f
tennispick <138655686+tennispick@users.noreply.github.com>
2024-02-11 18:55:29 +0900

    [TPFE-74] 회원 결제하기 퍼블리싱 (#11)
    * [TPFE-19] 스케줄 폴더 구조 리팩토링 (#1)
    
    * feat: additionalLesson, regularLesson developing
    
    * feat: commonFormInput developing
    
    * feat: 정규등록 InputForm 개발중
    
    * feat: folder structure devide
    
    ---------
    
    Co-authored-by: Dmon <rla99tjr@aitrics.com>
    Co-authored-by: tennispick <tennispick@gmail.com>
    
    * merge dev
    
    * 결제하기 1차 커밋
    
    * [TPFE-74] 회원 결제하기 퍼블리싱
    
    ---------
    
    Co-authored-by: Dmon <rla99tjr@aitrics.com>
    Co-authored-by: tennispick <tennispick@gmail.com>

---
+++ src/features/customer/component/modal/CustomerInfoContainer.tsx
@@ -0,0 +1,9 @@
+const CustomerModalCustomerInfoContainer = () => {
+  return(
+    <div css={{ position: 'relative', height: '130px', borderBottom: '1px solid var(--grey100)', padding: '0 28px',  }}>
+      인적정보
+    </div> 
+  )
+};
+
+export default CustomerModalCustomerInfoContainer;
