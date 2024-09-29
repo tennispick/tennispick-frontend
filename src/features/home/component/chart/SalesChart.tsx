a52e4a56ffeb8a16bb96d9ab36c3fd6a6e7aa853
KimGSeok <rla99tjr@gmail.com>
2024-09-29 17:33:21 +0900

    [TPFE-202] feat: 홈 대시보드 차트 반영

--- src/features/home/component/chart/SalesChart.tsx
+++ src/features/home/component/chart/SalesChart.tsx
@@ -1,8 +1,27 @@
 import { Flex } from 'styled-system/jsx';
 import Card from './Card';
 import { css } from 'styled-system/css';
+import { useHomeSalesStatisticsQuery } from '@features/home/query/statisticsQuery';
+import { addNumberCommas } from '@utils/numberForm';
 
-const SalesChart = () => {
+type Props = {
+  date: Date;
+};
+
+const SalesChart = ({ date }: Props) => {
+  const { isFetching, data } = useHomeSalesStatisticsQuery(date);
+
+  if (isFetching || !data) return null;
+
+  const { currentCustomer, totalSalesPrice } = data;
+  const { lastMonth, thisMonth } = currentCustomer;
+
+  const getIncreased = (lastMonth: number, thisMonth: number) => {
+    if (lastMonth < thisMonth) return 'Up';
+    if (lastMonth > thisMonth) return 'Down';
+    return 'NoChange';
+  };
+
   return (
     <div className={css({ width: 'calc(35% - 20px)' })}>
       <div
@@ -18,14 +37,14 @@ const SalesChart = () => {
         <Card
           title={'이번 달 매출금액'}
           subTitle={'1개월 전보다'}
-          chartType={'Up'}
-          content={'164명'}
+          chartType={getIncreased(lastMonth, thisMonth)}
+          content={`${addNumberCommas(thisMonth - lastMonth)} 원`}
         />
         <Card
           title={'누적 매출금액'}
           subTitle={'지금까지'}
           chartType={'NoChange'}
-          content={'999,999,999원'}
+          content={`${addNumberCommas(totalSalesPrice.paymentPrice)} 원`}
         />
       </Flex>
     </div>
